e2e00c42e8c29fc889d39f783a7a52a89829aa82
Luis Martinez <ernestox11@hotmail.com>
2022-07-21 05:08:41 -0400

    Remove unused import in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
