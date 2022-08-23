ac45725cfc8930c92ba85ca4f53e57f9a6c5c11c
Luis Martinez <ernestox11@hotmail.com>
2022-08-23 06:49:17 -0400

    Remove unused 'Destinations' module

--- src/destinations/dto/create-destination-input.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNumber } from 'class-validator';
-
-export class CreateDestinationInputDto {
-  @IsNumber()
-  latitude: number;
-
-  @IsNumber()
-  longitude: number;
-}
--- src/destinations/dto/update-destination-input.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateDestinationInputDto } from './create-destination-input.dto';
-
-export class UpdateDestinationInputDto extends PartialType(
-  CreateDestinationInputDto,
-) {}
