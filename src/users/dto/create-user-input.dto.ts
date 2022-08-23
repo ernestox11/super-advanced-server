4a7be0b40ebd95f8e069e5607cf037ff0edda9c6
Luis Martinez <ernestox11@hotmail.com>
2022-08-23 13:14:56 -0400

    Update 'create-user-input' dto

--- src/users/dto/create-user-input.dto.ts
+++ src/users/dto/create-user-input.dto.ts
@@ -9,30 +9,39 @@ import {
 
 export class CreateUserInputDto {
   @IsEmail()
+  @IsNotEmpty()
   email: string;
 
   @IsString()
+  @IsNotEmpty()
   password: string;
 
   @IsBoolean()
+  @IsNotEmpty()
   isActive: boolean;
 
   @IsString()
+  @IsNotEmpty()
   firstName: string;
 
   @IsString()
+  @IsNotEmpty()
   lastName: string;
 
   @IsString()
+  @IsNotEmpty()
   branchOffice: string;
 
   @IsString()
+  @IsNotEmpty()
   accessLevel: string;
 
   @IsString()
+  @IsNotEmpty()
   nationality: string;
 
   @IsNumber()
+  @IsNotEmpty()
   idNumber: number;
 
   @IsArray()
