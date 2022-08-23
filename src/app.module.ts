a66c82706dcec8eaae99c54f52350ac95e6b24fa
Luis Martinez <ernestox11@hotmail.com>
2022-08-23 08:10:48 -0400

    Create client resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,18 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { DestinationsModule } from './destinations/destinations.module';
+
 import { ConfigModule } from '@nestjs/config';
 import { ClientsModule } from './clients/clients.module';
+import { ClientModule } from './client/client.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_CONNECTION_STRING),
     UsersModule,
-    DestinationsModule,
     ClientsModule,
+    ClientModule,
   ],
   controllers: [AppController],
   providers: [AppService],
