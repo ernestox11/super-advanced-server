91667de7ce4b98ee7baba83577df5d726414ef4f
Luis Martinez <ernestox11@hotmail.com>
2022-08-27 18:12:02 -0400

    Add 'Travel Log' entity

--- src/main/travel-logs/entities/travel-log.entity.ts
+++ src/main/travel-logs/entities/travel-log.entity.ts
@@ -1,1 +1,15 @@
-export class TravelLog {}
+export class TravelLog {
+  originId: string;
+  destinationId: string;
+  estimatedTravelTime: number;
+  estimatedDeliveryTime: number;
+  log: [
+    {
+      orderID: string;
+      taskId: number;
+      arrivalDate: number;
+      duration: number;
+      deliveryTime: number;
+    },
+  ];
+}
