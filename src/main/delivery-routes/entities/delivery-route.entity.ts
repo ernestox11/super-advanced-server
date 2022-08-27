b772554c27435cef86ac29d982d58d6aa01cc672
Luis Martinez <ernestox11@hotmail.com>
2022-08-27 18:11:11 -0400

    Add 'Delivery Route' entity

--- src/main/delivery-routes/entities/delivery-route.entity.ts
+++ src/main/delivery-routes/entities/delivery-route.entity.ts
@@ -1,1 +1,22 @@
-export class DeliveryRoute {}
+import { Location } from '../../../common/schemas/location.schema';
+
+export class DeliveryRoute {
+  driverId: string;
+  vehicleId: string;
+  service: number;
+  duration: number;
+  waiting_time: number;
+  distance: number;
+  steps: [
+    {
+      type: string;
+      location: Location;
+      arrivalDate: number;
+      duration: number;
+      distance: number;
+      task?: number;
+      taskStatus?: string;
+      deliveryTime?: number;
+    },
+  ];
+}
