28032126e3521a230ac071c5d6079a232fc8dd87
Luis Martinez <ernestox11@hotmail.com>
2022-08-27 18:11:46 -0400

    Add 'Product' entity

--- src/main/products/entities/product.entity.ts
+++ src/main/products/entities/product.entity.ts
@@ -1,1 +1,11 @@
-export class Product {}
+export class Product {
+  productId: string;
+  name: string;
+  code: string;
+  category: string;
+  description: string;
+  weight: number;
+  length: number;
+  width: number;
+  height: number;
+}
