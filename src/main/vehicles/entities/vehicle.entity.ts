04f12931829a7a3ef984bad51ad3fd85deef87fd
Luis Martinez <ernestox11@hotmail.com>
2022-08-29 09:43:47 -0400

    Update 'Vehicle' entity

--- src/main/vehicles/entities/vehicle.entity.ts
+++ src/main/vehicles/entities/vehicle.entity.ts
@@ -1,6 +1,15 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({ timestamps: true })
 export class Vehicle {
+  @Prop({ type: String })
   vehicleId: string;
+
+  @Prop({ type: String })
   status: string;
+
+  @Prop({ type: String })
   branchOfficeId: string;
-  // model: VehicleModel;
 }
+
+export const ClientSchema = SchemaFactory.createForClass(Vehicle);
