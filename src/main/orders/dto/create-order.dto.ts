1df68ec77c487aed736369fd95d8d6093208cf91
Enrique Gimeno <enriquejgimeno@gmail.com>
2022-12-03 10:41:32 -0300

    Update create-order dto

--- src/main/orders/dto/create-order.dto.ts
+++ src/main/orders/dto/create-order.dto.ts
@@ -1,21 +1,29 @@
-import { IsNumber, IsString } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { Task } from '../entities/task.entity';
+import { CreateTaskDto } from './create-task-dto';
 
-export class CreateOrderDto {
+export class CreateOrderDto extends PartialType(CreateTaskDto) {
   @IsNumber()
+  @IsNotEmpty()
   orderNumber: number;
 
   @IsString()
+  @IsNotEmpty()
   clientId: string;
 
   @IsString()
+  @IsNotEmpty()
   branchOffice: string;
 
   @IsString()
+  @IsNotEmpty()
   receptionPointId: string;
 
   @IsString()
+  @IsNotEmpty()
   orderType: string;
 
+  @IsNotEmpty()
   tasks: [Task];
 }
